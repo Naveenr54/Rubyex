ad4a03294af121d334ec90906b28f2b0a4d8bec6
Naveen <122514015+Naveenr54@users.noreply.github.com>
2023-01-13 19:02:24 +0530

    Add files via upload

---
+++ 5exruby.rb
@@ -0,0 +1,17 @@
+his_name = 'selva'
+his_age = 23 
+his_height = 180 # cm
+his_weight = 90 # kg
+his_eyes = 'Brown'
+his_teeth = 'White'
+his_hair = 'Black'
+
+puts "Let's talk about #{his_name}."
+puts "He's #{his_height} cm tall."
+puts "He's #{his_weight} kg heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{his_eyes} eyes and #{his_hair} hair."
+puts "His teeth are usually #{his_teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{his_age}, #{his_height}, and #{his_weight} I get #{my_age + my_height + my_weight}."
