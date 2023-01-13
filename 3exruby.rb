d952dd5c6b654244724b79a42f20ec9eb3917cb4
Naveen <122514015+Naveenr54@users.noreply.github.com>
2023-01-13 15:41:26 +0530

    Add files via upload

---
+++ 3exruby.rb
@@ -0,0 +1,16 @@
+puts "no of fruits:"
+puts "apple #{45 + 55 / 2}"
+puts "orrange #{100 - 25 * 2 % 4}"
+
+puts "count the customers:"
+puts 5 + 10 -1 + 4 % 4 - 1 / 4 + 6
+puts "is it true that 5 + 4 > 5 - 2?"
+
+puts 4 + 5 > 9 - 8
+
+puts "What is 4 + 5? #{4 + 5}"
+puts "What is 9 - 8? #{9 - 8}"
+
+puts "Is it greater? #{10 > -4}"
+puts "Is it greater or equal? #{7 >= -3}"
+puts "Is it less or equal? #{8 <= -2}"
