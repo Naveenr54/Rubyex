5b9c913114c99522f3a73cad899912ec0be46041
Naveen <122514015+Naveenr54@users.noreply.github.com>
2023-01-13 18:50:08 +0530

    Add files via upload

---
+++ 4exruby.rb
@@ -0,0 +1,12 @@
+items = 100
+tomato = 150
+rottentomato = 25
+eggs = 20
+egg_needed_for_customer = 50
+
+
+puts "in our shop #{items} items available."
+puts "only #{eggs} eggs available."
+puts "but #{egg_needed_for_customer} eggs needed for customer."
+puts "there is a shortage of #{egg_needed_for_customer - eggs} left."
+puts "there are total of #{tomato + rottentomato} good and rottentomato."
