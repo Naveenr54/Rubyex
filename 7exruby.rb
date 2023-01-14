96884179edb368053680d4e7ee0b7ed0bbe77659
Naveen <122514015+Naveenr54@users.noreply.github.com>
2023-01-14 10:39:53 +0530

    Add files via upload

---
+++ 7exruby.rb
@@ -0,0 +1,22 @@
+puts "Harvey had a little dog."
+puts "it's one of the most funniest dog in the world."
+puts "And everywhere that Harvey went."
+puts "it follows back."
+puts "guess the dog name."
+puts "." * 20  # what'd that do?
+
+end1 = "l"
+end2 = "o"
+end3 = "u"
+end4 = "i"
+end5 = "s"
+end6 = "L"
+end7 = "i"
+end8 = "t"
+end9 = "t"
+end10 = "!"
+end11 = "!"
+end12 = "!"
+
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
