5c013f5373a0c1f39b158c664d4b0bc6dd0735f1
Naveen <122514015+Naveenr54@users.noreply.github.com>
2023-01-14 10:27:03 +0530

    Add files via upload

---
+++ 6exruby.rb
@@ -0,0 +1,21 @@
+diff_types_of_people = 10
+x = "There are #{diff_types_of_people} diff_types_of_people."
+code = "code"
+do_not = "don't"
+y = "Those who know #{code} and those who #{do_not}."
+
+puts x
+puts y
+
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+puts w + e
