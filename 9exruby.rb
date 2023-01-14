318e66672138ec73cd707e38b4dbeadc1dc07485
Naveen <122514015+Naveenr54@users.noreply.github.com>
2023-01-14 10:57:12 +0530

    Add files via upload

---
+++ 9exruby.rb
@@ -0,0 +1,8 @@
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+year = "365 days"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+puts "there are 12 months and #{year} in a year"
