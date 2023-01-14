745eb5ade1f30abbf47265d26656fcf3ef24406a
Naveen <122514015+Naveenr54@users.noreply.github.com>
2023-01-14 11:15:47 +0530

    Add files via upload

---
+++ 10exruby.rb
@@ -0,0 +1,19 @@
+  # escape diff btw double and single-quote inside string
+puts "i am 6'1\" tall."
+puts 'I am 6\'1" tall.' 
+
+tabby_guy = "\tI'm locked in."
+persian_guy = "I'm split\non a line."
+backslash_guy = "I'm \\ a \\ humanbeing."
+
+fat_guy = """
+I'll do a list:
+\t* saturated fat foods
+\t* Fishies
+\t* eggs\n\t* omega 3
+"""
+
+puts tabby_guy
+puts persian_guy
+puts backslash_guy
+puts fat_guy
