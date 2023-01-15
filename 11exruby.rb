68293a95716162389b33bbb6469e0c4b841b216c
Naveen <122514015+Naveenr54@users.noreply.github.com>
2023-01-15 09:43:42 +0530

    Add files via upload

---
+++ 11exruby.rb
@@ -0,0 +1,9 @@
+print "How old are you? "
+age = "22".chomp
+print "How tall are you? "
+height = "6.1".chomp
+print "How much do you weigh? "
+weight = "75".chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
