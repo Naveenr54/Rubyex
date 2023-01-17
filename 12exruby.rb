7b9050d67f99343aa875d2c8447b5d195fb5593e
Naveen <122514015+Naveenr54@users.noreply.github.com>
2023-01-17 13:32:00 +0530

    Update 12exruby.rb

--- 12exruby.rb
+++ 12exruby.rb
@@ -1,11 +1,11 @@
 print "Give me a number: "
-number = "10".chomp.to_i
+number = gets.chomp.to_i
 
 bigger = number * 100
 puts "A bigger number is #{bigger}."
 
 print "Give me another number: "
-another = "150".chomp
+another = gets.chomp
 number = another.to_i
 
 smaller = number / 100
